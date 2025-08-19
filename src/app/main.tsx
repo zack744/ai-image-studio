d41dcdd38f5983ad4e1b9dd8718df8106d971628
zackhe <zackhe774@gmail.com>
2025-08-19 09:41:22 +0800

    feat: add BProgress for router loading

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,3 +1,5 @@
+import '@bprogress/core/css';
+import { BProgress } from "@bprogress/core";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
@@ -11,8 +13,19 @@ import "./lib/polyfills";
 
 import "./index.css";
 
+BProgress.configure({
+	showSpinner: false,
+})
+
 // Create a new router instance
 const router = createRouter({ routeTree });
+router.subscribe("onBeforeLoad", ({ fromLocation, pathChanged }) => {
+	// Don't show the progress bar on initial page load, seems like the onLoad event doesn't fire in that case
+	fromLocation && pathChanged && BProgress.start();
+});
+router.subscribe("onLoad", () => {
+	BProgress.done();
+});
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
